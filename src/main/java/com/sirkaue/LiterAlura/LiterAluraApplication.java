bd6848343965c2ed3eeb4a43cb2a2fd036840a90
sirkaue <kauekdslopes@gmail.com>
2024-07-05 15:12:17 -0300

    main

--- src/main/java/com/sirkaue/LiterAlura/LiterAluraApplication.java
+++ src/main/java/com/sirkaue/LiterAlura/LiterAluraApplication.java
@@ -1,5 +1,9 @@
 package com.sirkaue.LiterAlura;
 
+import com.sirkaue.LiterAlura.menu.Menu;
+import com.sirkaue.LiterAlura.repository.AutorRepository;
+import com.sirkaue.LiterAlura.repository.LivroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,12 +11,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LiterAluraApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LiterAluraApplication.class, args);
-	}
+    @Autowired
+    private LivroRepository repository;
 
-	@Override
-	public void run(String... args) throws Exception {
+    @Autowired
+    private AutorRepository autorRepository;
 
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LiterAluraApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Menu menu = new Menu(autorRepository, repository);
+        menu.exibeMenu();
+    }
 }
